c1451c799adab7c828c163e8156796ff2fe27dd4
Martin Weismann <martin.weismann@autodesk.com>
2019-02-08 11:21:30 +0100

    Fix typo

--- mdToPDF.sh
+++ mdToPDF.sh
@@ -16,5 +16,5 @@ MARGIN=14
 
 ./wkhtmltopdf --title "$FILE" --footer-left "[section]" --footer-right "[page]/[topage]" --footer-font-size 7 --footer-spacing 2 \
 --margin-top $MARGIN --margin-left $MARGIN --margin-right $MARGIN --margin-bottom $MARGIN \
---zoom 1.2 --image-dpi 720\
+--zoom 1.2 --image-dpi 720 \
 "$TMPFILE" "$FILE.pdf"
